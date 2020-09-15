892357dffc70d613efe8b7d265e768b4122f8468
Artur Kovalchuk <artur.kovalchuk@worldapp.com>
2020-09-15 12:59:19 +0300

    Fix

--- src/main/java/com/example/demo/json/ResponseDto.java
+++ src/main/java/com/example/demo/json/ResponseDto.java
@@ -12,7 +12,7 @@ import java.time.Instant;
 public class ResponseDto<T> {
 
     private final int statusCode = 200;
-    private final Instant timeStamp = Instant.ofEpochMilli(1600162757613L);
+    private final long timeStamp = 1600162757602L;
     private final String message = "Ok";
     private String debugInfo;
     private String meta;
