60cce04afb60180750c4ea84ab976f4ca29e2810
Artur Kovalchuk <artur.kovalchuk@worldapp.com>
2020-09-15 14:00:09 +0300

    Fix lessonType

--- src/main/java/com/example/demo/json/lessons/LessonsConverter.java
+++ src/main/java/com/example/demo/json/lessons/LessonsConverter.java
@@ -41,6 +41,9 @@ public class LessonsConverter implements Converter<TimeTable, LessonsDto> {
                 .teachers(List.of(teacherDto))
                 .rooms(List.of(roomDto))
                 .dayName(requireNonNull(Day.findByKey(source.getDayNumber())).getName())
+                .lessonType("Лек")
+                .timeStart("10:25:00")
+                .timeEnd("12:00:00")
                 .build();
     }
 
